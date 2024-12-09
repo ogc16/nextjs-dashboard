69e12e9af067ea049f4516af72ae7c068cb233cc
Caleb Ngeno <82827770+ogc16@users.noreply.github.com>
2024-12-09 13:11:11 +0300

    Update route.ts
    removed

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -102,10 +102,6 @@ const client = await db.connect();
   }
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
     try {
       await client.sql`BEGIN`;
       await seedUsers();
