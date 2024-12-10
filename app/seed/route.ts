18b138437a865dfffd5a1b4553f21b9e2fc70c45
Caleb Ngeno <82827770+ogc16@users.noreply.github.com>
2024-12-10 12:45:01 +0300

    update

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,4 +1,4 @@
- import bcrypt from 'bcrypt';
+/*import bcrypt from 'bcrypt';
 import { db } from '@vercel/postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
@@ -116,3 +116,4 @@ export async function GET() {
       return Response.json({ error }, { status: 500 });
     }
 }
+*/
